0ddcb073fca68a163b234140a3780696caccfe86
Computer Wizard <mgasa.loucat1@gmail.com>
2023-02-16 21:00:15 +0300

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://mgasabot1.herokuapp.com/webhooks/rest/webhook";
+const rasa_server_url = "https://innovateyourself.herokuapp.com/webhooks/rest/webhook";
 const sender_id = uuidv4();
